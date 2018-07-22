dc5d70cbf4f62fce355d428f2dbafa0c523ddc8d
Anupom Roy <anupomr@yahoo.com>
2018-07-22 07:50:21 -0400

    Initial commit for the project

---
+++ app/src/main/java/com/example/anupo/anupom_bal_comp304_assign7/FirstActivity.java
@@ -0,0 +1,13 @@
+package com.example.anupo.anupom_bal_comp304_assign7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
